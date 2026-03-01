f67a6903120e6093472c460796e3846f554ab363
Jeremy Underwood <underwood.j248@gmail.com>
2026-03-01 18:56:22 -0500

    Use non-manifest WhiteNoise static storage

--- callen_site/settings.py
+++ callen_site/settings.py
@@ -160,7 +160,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
