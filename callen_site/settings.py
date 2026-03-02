f4ee6115ae8012902d8140d9ff7a2bfdd98fc7e0
Jeremy Underwood <underwood.j248@gmail.com>
2026-03-01 19:20:17 -0500

    Add cloudinary back to INSTALLED_APPS

--- callen_site/settings.py
+++ callen_site/settings.py
@@ -43,6 +43,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Application definition
 
 INSTALLED_APPS = [
+    "cloudinary",
     "cloudinary_storage",
     "django.contrib.admin",
     "django.contrib.auth",
