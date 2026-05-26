5e2ffd57415a7cf3c1dbcda548c50620433ba5bc
Jeremy Underwood <underwood.j248@gmail.com>
2026-05-25 21:36:55 -0400

    Main Change 1

--- callen_site/settings.py
+++ callen_site/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os, dj_database_url
 from dotenv import load_dotenv
-
+# Main Change 1
 load_dotenv()  # reads .env from the project root
 # print("DB:", os.getenv("DB_USER"), os.getenv("DB_NAME"))
 
